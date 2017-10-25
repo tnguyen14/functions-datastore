a6b32704f0b0a50cd719df8c1239f993ef74dab6
Tri Nguyen <tri@tridnguyen.com>
2017-10-25 01:06:01 -0400

    add jwt authentication check

--- index.js
+++ index.js
@@ -16,10 +16,74 @@
 'use strict';
 
 const Datastore = require('@google-cloud/datastore');
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
 
 // Instantiates a client
 const datastore = Datastore();
 
+const client = jwksClient({
+  cache: true,
+  rateLimit: true,
+  jwksRequestsPerMinute: 5,
+  jwksUri: "https://tridnguyen.auth0.com/.well-known/jwks.json"
+});
+
+function verifyToken(token, cb) {
+  let decodedToken;
+  try {
+    decodedToken = jwt.decode(token, {complete: true});
+  } catch (e) {
+    console.error(e);
+    cb(e);
+    return;
+  }
+  client.getSigningKey(decodedToken.header.kid, function (err, key) {
+    if (err) {
+      console.error(err);
+      cb(err);
+      return;
+    }
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    jwt.verify(token, signingKey, function (err, decoded) {
+      if (err) {
+        console.error(err);
+        cb(err);
+        return
+      }
+      console.log(decoded);
+      cb(null, decoded);
+    });
+  });
+}
+
+function checkAuth (fn) {
+  return function (req, res) {
+    if (!req.headers || !req.headers.authorization) {
+      res.status(401).send('No authorization token found.');
+      return;
+    }
+    const parts = req.headers.authorization.split(' ');
+    if (parts.length != 2) {
+      res.status(401).send('Bad credential format.');
+      return;
+    }
+    const scheme = parts[0];
+    const credentials = parts[1];
+
+    if (!/^Bearer$/i.test(scheme)) {
+      res.status(401).send('Bad credential format.');
+      return;
+    }
+    verifyToken(credentials, function (err) {
+      if (err) {
+        res.status(401).send('Invalid token');
+        return;
+      }
+      fn(req, res);
+    });
+  };
+}
 /**
  * Gets a Datastore key from the kind/key pair in the request.
  *
@@ -53,7 +117,7 @@ function getKeyFromRequestData (requestData) {
  * @param {object} req.body.value Value to save to Cloud Datastore, e.g. {"description":"Buy milk"}
  * @param {object} res Cloud Function response context.
  */
-exports.set = function set (req, res) {
+function set (req, res) {
   // The value contains a JSON document representing the entity we want to save
   if (!req.body.value) {
     throw new Error('Value not provided. Make sure you have a "value" property in your request');
@@ -74,6 +138,8 @@ exports.set = function set (req, res) {
     });
 };
 
+exports.set = checkAuth(set);
+
 /**
  * Retrieves a record.
  *
@@ -86,7 +152,7 @@ exports.set = function set (req, res) {
  * @param {string} req.body.key Key at which to retrieve the data, e.g. "sampletask1".
  * @param {object} res Cloud Function response context.
  */
-exports.get = function get (req, res) {
+function get (req, res) {
   const key = getKeyFromRequestData(req.body);
 
   return datastore.get(key)
@@ -104,7 +170,9 @@ exports.get = function get (req, res) {
       res.status(500).send(err);
       return Promise.reject(err);
     });
-};
+}
+
+exports.get = checkAuth(get);
 
 /**
  * Deletes a record.
