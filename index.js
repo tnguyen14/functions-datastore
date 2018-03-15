e2cd1b13d35b610f1f6f14e4e49f6856dbe5b879
Tri Nguyen <tri@tridnguyen.com>
2018-03-14 23:01:52 -0400

    abstract auth to its own module

--- index.js
+++ index.js
@@ -15,75 +15,12 @@
 
 'use strict';
 
+const checkAuth = require('./auth');
 const Datastore = require('@google-cloud/datastore');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
 
 // Instantiates a client
 const datastore = Datastore();
 
-const client = jwksClient({
-  cache: true,
-  rateLimit: true,
-  jwksRequestsPerMinute: 5,
-  jwksUri: "https://tridnguyen.auth0.com/.well-known/jwks.json"
-});
-
-function verifyToken(token, cb) {
-  let decodedToken;
-  try {
-    decodedToken = jwt.decode(token, {complete: true});
-  } catch (e) {
-    console.error(e);
-    cb(e);
-    return;
-  }
-  client.getSigningKey(decodedToken.header.kid, function (err, key) {
-    if (err) {
-      console.error(err);
-      cb(err);
-      return;
-    }
-    const signingKey = key.publicKey || key.rsaPublicKey;
-    jwt.verify(token, signingKey, function (err, decoded) {
-      if (err) {
-        console.error(err);
-        cb(err);
-        return
-      }
-      console.log(decoded);
-      cb(null, decoded);
-    });
-  });
-}
-
-function checkAuth (fn) {
-  return function (req, res) {
-    if (!req.headers || !req.headers.authorization) {
-      res.status(401).send('No authorization token found.');
-      return;
-    }
-    const parts = req.headers.authorization.split(' ');
-    if (parts.length != 2) {
-      res.status(401).send('Bad credential format.');
-      return;
-    }
-    const scheme = parts[0];
-    const credentials = parts[1];
-
-    if (!/^Bearer$/i.test(scheme)) {
-      res.status(401).send('Bad credential format.');
-      return;
-    }
-    verifyToken(credentials, function (err) {
-      if (err) {
-        res.status(401).send('Invalid token');
-        return;
-      }
-      fn(req, res);
-    });
-  };
-}
 /**
  * Gets a Datastore key from the kind/key pair in the request.
  *
@@ -186,7 +123,7 @@ exports.get = checkAuth(get);
  * @param {string} req.body.key Key at which to delete data, e.g. "sampletask1".
  * @param {object} res Cloud Function response context.
  */
-exports.del = function del (req, res) {
+function del (req, res) {
   const key = getKeyFromRequestData(req.body);
 
   // Deletes the entity
@@ -198,3 +135,5 @@ exports.del = function del (req, res) {
       return Promise.reject(err);
     });
 };
+
+exports.del = checkAuth(del);
